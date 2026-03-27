a3e119df1b142f059d082dd417986432704fe0ee
Ilann Thourault <ilann.thourault@gmail.com>
2026-03-27 15:19:51 +0100

    pom.xml + main.java (plus propre que lancer direct sur app.java)

---
+++ src/main/java/com/lmu/SlitherThink/Main.java
@@ -0,0 +1,9 @@
+package com.lmu.SlitherThink;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
