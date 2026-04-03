36777ee85bda84938bad068158ae63e42739eae8
tibogas <tibo.gasnier@gmail.com>
2026-04-03 11:30:11 +0200

    Changement du gestinaire d'aide adapté aux nouveaux noms de fichiers

--- src/main/java/com/lmu/SlitherThink/Helper/GestionnaireAide.java
+++ src/main/java/com/lmu/SlitherThink/Helper/GestionnaireAide.java
@@ -40,10 +40,10 @@ public class GestionnaireAide {
         strategies.add(new ContraintesSur2());
 
         //Aide confirme
-        strategies.add(new TechniqueAvancee1());
-        strategies.add(new TechniqueAvancee2());
+        strategies.add(new ContinuiteForcee());
+        strategies.add(new CoinsBloquesSurUn3());
         strategies.add(new Symetrie2Adjacents());
-        strategies.add(new TechniqueAvancee5());
+        strategies.add(new Diagonale1Et3());
 
         //Aide fin de partie
         strategies.add(new EviterBoucleSeparee());
